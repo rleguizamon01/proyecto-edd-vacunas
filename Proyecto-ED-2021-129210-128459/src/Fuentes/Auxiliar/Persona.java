1af0d60987e7d5fadcef6c803e7eeefc45e79b94
octavioantich <62580250+octavioantich@users.noreply.github.com>
2021-05-31 22:29:51 -0300

    i added el CONSTRUCTOR, OBTENEDORES y ESTABLECEDORES to la clase persona

--- Proyecto-ED-2021-129210-128459/src/Fuentes/Auxiliar/Persona.java
+++ Proyecto-ED-2021-129210-128459/src/Fuentes/Auxiliar/Persona.java
@@ -1,5 +1,35 @@
 package Auxiliar;
 
 public class Persona {
+	private String nombre;
+	private String apellido;
+	private int DNI;
+	
+	
+	
+	public Persona(String nombre, String apellido, int dni) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		DNI = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public int getDNI() {
+		return DNI;
+	}
+	public void setDNI(int dNI) {
+		DNI = dNI;
+	}
 
 }
